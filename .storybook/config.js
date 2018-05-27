8d61681391b9464751d8494162e42c2cea1e74b6
Jack Tan <jacktan165@gmail.com>
2018-05-28 00:54:43 +1200

    Added centered, a11y, actions and knobs storybook add ons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,16 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import centered from "@storybook/addon-centered";
+import { checkA11y } from "@storybook/addon-a11y";
+import { withKnobs } from "@storybook/addon-knobs";
 
 const req = require.context("../src", true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(centered);
+addDecorator(checkA11y);
+addDecorator(withKnobs);
 
 configure(loadStories, module);
