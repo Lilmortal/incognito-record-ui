4f233aa929604450ad991680d3c5dadbc382c367
Jack Tan <jacktan165@gmail.com>
2018-06-07 00:41:40 +1200

    WIP; categories now transition

--- src/categories/Categories.js
+++ src/categories/Categories.js
@@ -1,44 +1,46 @@
 import React from "react";
-import { Trail } from "react-spring";
+import { Trail, animated } from "react-spring";
 
 import createBem from "../util/createBem";
 import "./Categories.scss";
 
 const bem = createBem("incognito-categories");
 
-export default class Categories extends React.Component {
+export default class Categories extends React.PureComponent {
   state = {
-    hovered: false
+    toggle: false
   };
 
   handleMouseHover = () => {
-    this.setState({ hovered: true });
+    this.setState({ toggle: true });
   };
 
   handleMouseLeave = () => {
-    this.setState({ hovered: false });
+    this.setState({ toggle: false });
   };
 
   render() {
-    const { hovered } = this.state;
+    const { toggle } = this.state;
     const { categories } = this.props;
 
     // TODO: Can choose between marginLeft and marginRight
     return (
       <div className={bem()} onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseLeave}>
-        {
-          <Trail
-            from={{ opacity: 0, marginRight: -100 }}
-            to={{ opacity: hovered ? 1 : 0.5, marginRight: hovered ? 100 : 0 }}
-            keys={categories.map(category => category.key)}
-          >
-            {categories.map(category => styles => (
-              <div className={bem("category")} style={styles}>
-                {category.text}
-              </div>
-            ))}
-          </Trail>
-        }
+        <Trail
+          native
+          from={{ x: 100, opacity: 0.4 }}
+          to={{ x: toggle ? -100 : 100, opacity: toggle ? 1 : 0.4 }}
+          keys={categories.map(category => category.key)}
+        >
+          {categories.map(category => ({ x, opacity }) => (
+            <animated.div
+              className={bem("category")}
+              style={{ opacity, transform: x.interpolate(marginRight => `translateX(${marginRight}%)`) }}
+            >
+              {category.text}
+            </animated.div>
+          ))}
+        </Trail>
       </div>
     );
   }
