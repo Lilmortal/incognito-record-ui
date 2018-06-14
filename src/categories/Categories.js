2dc8d52a1518f345e01eb9fbfb802247ba363469
Jack Tan <Jack.Tan@airnz.co.nz>
2018-06-14 17:30:14 +1200

    Got react spring keyframes sort of working

--- src/categories/Categories.js
+++ src/categories/Categories.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Trail, animated } from "react-spring";
+import { Keyframes, config, animated } from "react-spring";
 
 import createBem from "../util/createBem";
 import "./Categories.scss";
 
 const bem = createBem("incognito-categories");
+
+const Container = Keyframes.Spring({
+  show: {
+    to: {
+      t: 0
+    }
+  },
+  hide: {
+    to: {
+      t: 100
+    }
+  }
+})
+
+const Options = Keyframes.Trail({
+  show: {
+    to: {
+      x: 0
+    }
+  },
+  hide: {
+    to: {
+      x: 100
+    }
+  }
+})
 
 export default class Categories extends React.PureComponent {
   state = {
@@ -20,29 +46,40 @@ export default class Categories extends React.PureComponent {
   };
 
   render() {
-    const { toggle } = this.state;
+    // const { toggle } = this.state;
     const { categories } = this.props;
 
     // TODO: Can choose between marginLeft and marginRight
     return (
       <div className={bem()} onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseLeave}>
-        <div className={bem("categoriesWrapper")}>
-          <Trail
-            native
-            from={{ x: 100, opacity: 0.4 }}
-            to={{ x: toggle ? 0 : 100, opacity: toggle ? 1 : 0.4 }}
-            keys={categories.map(category => category.key)}
-          >
-            {categories.map(category => ({ x, opacity }) => (
-              <animated.div
-                className={bem("category")}
-                style={{ opacity, transform: x.interpolate(marginRight => `translateX(${marginRight}%)`) }}
-              >
-                {category.text}
-              </animated.div>
-            ))}
-          </Trail>
-        </div>
+        <Container native state={this.state.toggle ? 'show' : 'hide'}>
+          {styles => {
+              console.log(styles)
+            
+              return (
+                <animated.div className={bem('categoriesWrapper')} style={{ transform: styles.t.interpolate(marginRight => `translateX(${marginRight}%)`)}}>
+                  <Options
+                    native
+            // from={{ x: 100, opacity: 0.4 }}
+            // to={{ x: toggle ? 0 : 100, opacity: toggle ? 1 : 0.4 }}
+                    keys={categories.map(category => category.key)}
+                    state={this.state.toggle ? 'show' : 'hide'}
+                  >
+                    {categories.map(category => ({ x }) => {
+                      console.log(x);
+                      return (
+                        <animated.div
+                          className={bem("category")}
+                          style={{ transform: x.interpolate(marginRight => `translateX(${marginRight}%)`) }}
+                        >
+                          {category.text}
+                        </animated.div>
+                      )})}
+                  </Options>
+                </animated.div>
+                )}}
+            
+        </Container>
       </div>
     );
   }
