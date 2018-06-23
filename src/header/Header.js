2597e6f372265076829c86a29b55a5e10a40dcfc
Jack Tan <jacktan165@gmail.com>
2018-06-23 13:48:22 +1200

    Move contents to its own component directory

--- src/header/Header.js
+++ src/header/Header.js
@@ -12,25 +12,28 @@ const bem = createBem("incognito-header");
 const Header = ({ isHomePage }) => (
   <header className={bem()}>
     <Navigation />
-    <div className={bem("search")}>
-      <Search />
-    </div>
+
     {isHomePage && (
-      <div className={bem("categories")}>
-        <Navicon />
-        <Categories
-          categories={[
-            { key: 0, text: "Docker" },
-            { key: 1, text: "Node JS" },
-            { key: 2, text: "Golang" },
-            { key: 3, text: "Kubernetes" },
-            { key: 4, text: "Terraform" },
-            { key: 5, text: "AWS Lambda" },
-            { key: 6, text: "AWS Cloudfront" },
-            { key: 7, text: "Elastic Search" }
-          ]}
-        />
-      </div>
+      <React.Fragment>
+        <div className={bem("search")}>
+          <Search />
+        </div>
+        <div className={bem("categories")}>
+          <Navicon />
+          <Categories
+            categories={[
+              { key: 0, text: "Docker" },
+              { key: 1, text: "Node JS" },
+              { key: 2, text: "Golang" },
+              { key: 3, text: "Kubernetes" },
+              { key: 4, text: "Terraform" },
+              { key: 5, text: "AWS Lambda" },
+              { key: 6, text: "AWS Cloudfront" },
+              { key: 7, text: "Elastic Search" }
+            ]}
+          />
+        </div>
+      </React.Fragment>
     )}
   </header>
 );
--- src/page/about/About.js
+++ src/page/about/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Transition, animated } from "react-spring";
 
+import Content from "../../content";
 import createBem from "../../util/createBem";
 import "./About.scss";
 
@@ -13,7 +14,7 @@ export default class About extends React.Component {
         const { isIntersecting } = entry;
 
         if (isIntersecting) {
-          entry.target.className = bem("contentTitle", "isIntersecting");
+          entry.target.className += ` ${entry.target.className}--isIntersecting`;
         }
       });
     });
@@ -21,24 +22,13 @@ export default class About extends React.Component {
 
   pushContentRefs = ref => this.observer.observe(ref);
 
-  Content = ({ title, children }) => (
-    <div className={bem("contentWrapper")}>
-      <div className={bem("contentTitleWrapper")}>
-        <h2 className={bem("contentTitle")} ref={this.pushContentRefs}>
-          {title}
-        </h2>
-      </div>
-      <div className={bem("content")}>{children}</div>
-    </div>
-  );
-
   render() {
     const contents = [];
     for (let i = 0; i < 200; i += 1) {
       contents.push(
-        <this.Content key={i} title="Title">
+        <Content key={i} title="Title" onRefObserve={this.pushContentRefs}>
           About
-        </this.Content>
+        </Content>
       );
     }
 
