68e33f297ef58d7cc6f8a1f85def3aa15fe1b55c
Jack Tan <jacktan165@gmail.com>
2018-07-14 18:06:39 +1200

    Fix root page not being found

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import Root from "./page/root";
+import Root from "./page/Root";
 
 const Index = () => (
   <BrowserRouter>
