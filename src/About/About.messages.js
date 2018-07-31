3cf06e6f7dbe79b47f00d36710b854fe76740a7c
Jack Tan <Jack.Tan@airnz.co.nz>
2018-07-31 12:07:28 +1200

    Update about messages

--- src/About/About.messages.js
+++ src/About/About.messages.js
@@ -1,70 +1,70 @@
 export default {
   aboutMeTitle: {
     id: 'incognito.About.aboutMeTitle',
-    defaultMessage: 'About me'
+    defaultMessage: 'About me',
   },
   aboutMeContent: {
     id: 'incognito.About.aboutMeContent',
     defaultMessage: `
-    Hey all, my name is Jack. This website is made from scratch by me to showcase my progression towards being a competent full stack developer. I will be showcasing what I have learnt so far, as well as detailing what do I still need to improve on. 
+    Hey all, my name is Jack. This website is made from scratch by me to kinda practice and be a personal blog for me to input what I have learnt. 
     
-    The code for this website is found in this github link: {link}. At the moment, it has most of the foundations for i18n, a11y, which I am planning to resolve in the coming weeks.`
+    The code for this website is found in this github link: {link}. At the moment, it has most of the foundations for i18n, a11y, which I am planning to resolve in the coming weeks.`,
   },
   whyAmIDoingThisTitle: {
     id: 'incognito.About.whyAmIDoingThisTitle',
-    defaultMessage: 'Why am I doing this?'
+    defaultMessage: 'Why am I doing this?',
   },
   whyAmIDoingThisContent: {
     id: 'incognito.About.whyAmIDoingThisContent',
     defaultMessage: `
-    I am severely suffering (lol, suffering may be the wrong word here) from imposter syndrome. There are still some basic concepts that I do not yet fully understand.`
+    There are still some basic concepts that I do not yet fully understand. When I learn something, I tend to forget it after a few months. So I'm going to put what I have learn here.`,
   },
   whatAmITryingToAchieveTitle: {
     id: 'incognito.About.whatAmITryingToAchieveTitle',
-    defaultMessage: 'What am I trying to achieve?'
+    defaultMessage: 'What am I trying to achieve?',
   },
   whatAmITryingToAchieveContent: {
     id: 'incognito.About.whatAmITryingToAchieveContent',
     defaultMessage: `
-    Gain a thorough understanding of software development in general; at the moment, focusing particularly on the web. This includes system design which I am heavily focusing on at the moment; and performances, particularly Big O notations (time and space complexity). On the web, front end, back end, deployment, and some database skills. I have wrote down a list of concepts in my own room that I started few years ago, I am intending to finish it.
+    Gain a thorough understanding of software development in general; at the moment, focusing particularly on the web. This includes system design which I am heavily focusing on at the moment; and performances, particularly Big O notations (time and space complexity). Also a thorough understanding of the web, (front end, back end, deployment, and some database skills). I have wrote down a list of concepts in my own room that I started few years ago, I am intending to finish it.
     
-    The first to get it out of the way is I do not have much experiences in any of the cloud features, e.g. AWS. I have basic experiences in AWS S3, Cloudfront, Route 53, EC2, DynamoDB, and RDS. The more advanced topics I can catch up on in my 9-5 work. In the coming weeks, I will be writing about CNames, Alias records, EBS as well as EFS.
+    The first thing to get it out of the way is I do not have much experiences in any of the cloud features, e.g. AWS. I have basic experiences in AWS S3, Cloudfront, Route 53, EC2, and RDS. The more advanced topics I will put what I have learn here. In the coming weeks, I will be writing about CNames, Alias records, EBS as well as EFS.
 
-    I still lack basic understanding of networking in general, (e.g. subnets etc). AWS VPC, VPN etc; as well as Cloudformation or Terraform.`
+    I still lack basic understanding of networking in general, (e.g. subnets etc). AWS VPC, VPN etc; as well as Cloudformation or Terraform.`,
   },
   howAmIGoingToAchieveThisTitle: {
     id: 'incognito.About.howAmIGoingToAchieveThisTitle',
-    defaultMessage: 'How am I going to achieve this?'
+    defaultMessage: 'How am I going to achieve this?',
   },
   howAmIGoingToAchieveThisContent: {
     id: 'incognito.About.howAmIGoingToAchieveThisContent',
     defaultMessage: `
-    I will be posting what I have learnt, and what project I am currently doing that will help support this. Large companies typically conduct interviews where you are expected to think out loud.
+    I will be posting what I have learnt, and what project I am currently doing that will help support this.
       
-    I will be posting my solutions to coding questions and how I reach that conclusion. This will be beneficial for me for the upcoming decades as I will forget stuff, but reading this posts will help me remember it. Coding questions will typically come from Cracking the Coding Interview book and https://www.dailycodingproblem.com`
+    I will be posting my solutions to coding questions and how I reach that conclusion. This will be beneficial for me for the upcoming decades as I will forget stuff, but reading this posts will help me remember it. Coding questions will typically come from Cracking the Coding Interview book and https://www.dailycodingproblem.com`,
   },
   whatIsMyDeadlineTitle: {
     id: 'incognito.About.whatIsMyDeadlineTitle',
-    defaultMessage: 'What is my deadline?'
+    defaultMessage: 'What is my deadline?',
   },
   frontEndButtonText: {
     id: 'incognito.About.frontEndButtonText',
-    defaultMessage: 'Front end'
+    defaultMessage: 'Front end',
   },
   backEndButtonText: {
     id: 'incognito.About.backEndButtonText',
-    defaultMessage: 'Back end'
+    defaultMessage: 'Back end',
   },
   devOpsButtonText: {
     id: 'incognito.About.devOpsButtonText',
-    defaultMessage: 'Dev ops'
+    defaultMessage: 'Dev ops',
   },
   algorithmsButtonText: {
     id: 'incognito.About.algorithmsButtonText',
-    defaultMessage: 'Algorithms and wrapping up'
+    defaultMessage: 'Algorithms',
   },
   finalCountdownTitle: {
     id: 'incognito.About.finalCountdownTitle',
-    defaultMessage: 'Countdown till my deadline'
-  }
+    defaultMessage: 'Countdown till my deadline',
+  },
 };
